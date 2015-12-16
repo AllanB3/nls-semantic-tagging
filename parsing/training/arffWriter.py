64165bb57c6651cd564ff787004759f00fcfbe37
Allan Brown <allanbrown239@gmail.com>
2015-12-16 16:15:53 +0000

    Changed annotation and ARFF writer to handle the new title class of token.

--- parsing/training/arffWriter.py
+++ parsing/training/arffWriter.py
@@ -8,7 +8,7 @@ import re
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(1, path)
 
-from xmlparser import *
+from xmlParser import *
 
 trainingFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), sys.argv[1]))
 trainingOutputPath = os.path.abspath(os.path.join(os.path.dirname(__file__), sys.argv[2]))
@@ -37,32 +37,36 @@ for fileName in os.listdir(trainingFolder):
         digits = 0
         dictionaryTags = []
 
-        featureVector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        featureVector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         if len(tagsAndVectors) > 0:
-            previousValue = tagsAndVectors[len(tagsAndVectors) - 1][11]
+            previousValue = tagsAndVectors[len(tagsAndVectors) - 1][12]
 
             if previousValue == "SURNAME":
                 featureVector[0] = 1
             elif previousValue == "FORENAME":
                 featureVector[1] = 1
-            elif previousValue == "OCCUPATION":
+            elif previousValue == "TITLE":
                 featureVector[2] = 1
-            elif previousValue == "WORK_ADDRESS":
+            elif previousValue == "OCCUPATION":
                 featureVector[3] = 1
-            elif previousValue == "HOME_ADDRESS":
+            elif previousValue == "ADDRESS":
                 featureVector[4] = 1
 
-        featureVector[6] = len(re.findall("\d", token))
+        featureVector[5] = len(re.findall("\d", token))
 
         tag = re.search(r"\[[A-Z|_]*\]", token).group()
         tag = tag[1 : len(tag) - 1]
+
+        # TODO: change these in annotations
+        if tag == "WORK_ADDRESS" or tag == "HOME_ADDRESS":
+            tag = "ADDRESS"
 
         token = re.sub(r"\[(\s|\n)*[A-Z|_]*\]\)", "", token)
         token = re.sub(r"^\(", "", token)
         token = re.sub(r"\d+\s*", "", token)
         print(token)
-        featureVector[5] = len(token)
+        featureVector[6] = len(token)
 
         lexicon = open("classifierTraining.txt", "r").read()
         entries = lexicon.splitlines()
@@ -75,10 +79,12 @@ for fileName in os.listdir(trainingFolder):
                     featureVector[7] = 1
                 elif dictTag == "forename":
                     featureVector[8] = 1
-                elif dictTag == "occupation":
+                elif dictTag == "title":
                     featureVector[9] = 1
-                elif dictTag == "address":
+                elif dictTag == "occupation":
                     featureVector[10] = 1
+                elif dictTag == "address":
+                    featureVector[11] = 1
 
         featureVector.append(tag)
         print(featureVector)
@@ -88,18 +94,19 @@ for fileName in os.listdir(trainingFolder):
 
 arffWriter = arff.Writer(trainingOutputPath, relation="postOfficeData", names=['previousSurname',
                                                                                'previousForename',
+                                                                               'previousTitle',
                                                                                'previousOccupation',
-                                                                               'previousWorkAddress',
-                                                                               'previousHomeAddress',
+                                                                               'previousAddress',
                                                                                'tokenLength',
                                                                                'digits',
                                                                                'dictionarySurname',
                                                                                'dictionaryForename',
+                                                                               'dictionaryTitle',
                                                                                'dictionaryOccupation',
                                                                                'dictionaryAddress',
                                                                                'class'])
 
-arffWriter.pytypes[str] = '{SURNAME, FORENAME, OCCUPATION, WORK_ADDRESS, HOME_ADDRESS}'
+arffWriter.pytypes[str] = '{SURNAME, FORENAME, TITLE, OCCUPATION, ADDRESS}'
 
 
 for data in tagsAndVectors:
