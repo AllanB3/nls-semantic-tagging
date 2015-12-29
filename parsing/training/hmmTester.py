a22fe92f17597e935a3b191badbddd5defbf29f5
s1224425 <s1224425@sms.ed.ac.uk>
2015-12-29 17:30:47 +0000

    Did more work on the HMM tester. Still need to figure out specifics of aligning BRAT output and HMM output.

--- parsing/training/hmmTester.py
+++ parsing/training/hmmTester.py
@@ -39,14 +39,89 @@ for dirName in os.listdir(TESTINGFOLDER):
         fileExtension = testFile[-3:]
 
         if fileExtension == "xml":
-            xmlFile = os.path.join(TESTINGFOLDER, testFile)
+            xmlFile = os.path.join(os.path.join(TESTINGFOLDER, dirName), testFile)
         elif fileExtension == "ann":
-            annFile = os.path.join(TESTINGFOLDER, testFile)
+            annFile = os.path.join(os.path.join(TESTINGFOLDER, dirName), testFile)
 
     if xmlFile == "" or annFile == "":
         raise IOError("Test folder must have an XML file of the page from the NLS and a corresponding brat ann file.")
 
     text = x.parseNLSPage(xmlFile)
-    tags = hmm.tag(text)
+    classifiedTags = hmm.tag(text)
 
-    # TODO: figure out a way to align tags from the HMM and tags from brat.
+    annFileText = open(annFile, "r").read().splitlines()
+
+    hmmCounter = 0
+    for a in annFileText:
+        key, tokenData, actualToken = a.split("\t")
+
+        classifiedToken = ""
+        classifiedTag = ""
+        while classifiedToken != actualToken:
+            classifiedToken, classifiedTag = classifiedTags[hmmCounter]
+            hmmCounter += 1
+
+        actualTag = tokenData.split()[0].upper()
+
+        confusionMatrix[actualTag][classifiedTag] += 1
+
+        if actualTag == classifiedTag:
+            results[actualTag]["TP"] += 1
+
+            for tag, tagResults in results.items():
+                if tag != actualTag:
+                    tagResults["TN"] += 1
+        else:
+            results[actualTag]["FN"] += 1
+            results[classifiedTag]["FP"] += 1
+
+            for tag, tagResults in results.items():
+                if tag != actualTag and tag != classifiedTag:
+                    tagResults["TN"] += 1
+
+scores = {"SURNAME": {"Precision": 0, "Recall": 0, "F-score": 0},
+          "FORENAME": {"Precision": 0, "Recall": 0, "F-score": 0},
+          "TITLE": {"Precision": 0, "Recall": 0, "F-score": 0},
+          "OCCUPATION": {"Precision": 0, "Recall": 0, "F-score": 0},
+          "ADDRESS": {"Precision": 0, "Recall": 0, "F-score": 0}}
+
+for tag, tagResults in results.items():
+    precision = tagResults["TP"] / (tagResults["TP"] + tagResults["FP"])
+    scores[tag]["Precision"] = precision
+
+    recall = tagResults["TP"] / (tagResults["TP"] + tagResults["FN"])
+    scores[tag]["Recall"] = recall
+
+    scores[tag]["F-score"] = (2 * precision * recall) / (precision + recall)
+
+table = [["", "SURNAME", "FORENAME", "TITLE", "OCCUPATION", "ADDRESS"],
+         ["Precision", scores["SURNAME"]["Precision"], scores["FORENAME"]["Precision"],
+          scores["TITLE"]["Precision"], scores["OCCUPATION"]["Precision"],
+          scores["ADDRESS"]["Precision"]],
+         ["Recall", scores["SURNAME"]["Recall"], scores["FORENAME"]["Recall"],
+          scores["TITLE"]["Recall"], scores["OCCUPATION"]["Recall"],
+          scores["ADDRESS"]["Recall"]],
+         ["F-score", scores["SURNAME"]["F-score"], scores["FORENAME"]["F-score"],
+          scores["TITLE"]["F-score"], scores["OCCUPATION"]["F-score"],
+          scores["ADDRESS"]["F-score"]]]
+
+confusion = [["", "SURNAME", "FORENAME", "TITLE", "OCCUPATION", "ADDRESS"],
+             ["SURNAME", confusionMatrix["SURNAME"]["SURNAME"], confusionMatrix["SURNAME"]["FORENAME"],
+              confusionMatrix["SURNAME"]["TITLE"], confusionMatrix["SURNAME"]["OCCUPATION"],
+              confusionMatrix["SURNAME"]["ADDRESS"]],
+             ["FORENAME", confusionMatrix["FORENAME"]["SURNAME"], confusionMatrix["FORENAME"]["FORENAME"],
+              confusionMatrix["FORENAME"]["TITLE"], confusionMatrix["FORENAME"]["OCCUPATION"],
+              confusionMatrix["FORENAME"]["ADDRESS"]],
+             ["TITLE", confusionMatrix["TITLE"]["SURNAME"], confusionMatrix["TITLE"]["FORENAME"],
+              confusionMatrix["TITLE"]["TITLE"], confusionMatrix["TITLE"]["OCCUPATION"],
+              confusionMatrix["TITLE"]["ADDRESS"]],
+             ["OCCUPATION", confusionMatrix["OCCUPATION"]["SURNAME"], confusionMatrix["OCCUPATION"]["FORENAME"],
+              confusionMatrix["OCCUPATION"]["TITLE"], confusionMatrix["OCCUPATION"]["OCCUPATION"],
+              confusionMatrix["OCCUPATION"]["ADDRESS"]],
+             ["ADDRESS", confusionMatrix["ADDRESS"]["SURNAME"], confusionMatrix["ADDRESS"]["FORENAME"],
+              confusionMatrix["ADDRESS"]["TITLE"], confusionMatrix["ADDRESS"]["OCCUPATION"],
+              confusionMatrix["ADDRESS"]["ADDRESS"]]]
+
+print(tabulate(table))
+print("")
+print(tabulate(confusion))
