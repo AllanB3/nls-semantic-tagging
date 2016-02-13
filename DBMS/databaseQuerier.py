32143736e39eb4f8f5d19755a4d2141fc86eb468
Allan Brown <allanbrown239@gmail.com>
2016-02-13 17:53:35 +0000

    Database querier now returns records. Need to figure out a way to include field names.

--- DBMS/databaseQuerier.py
+++ DBMS/databaseQuerier.py
@@ -16,12 +16,12 @@ class DatabaseQuerier:
 
     # TODO: fix syntax error with query
     def query(self, surname=None, forename=None, title=None, occupation=None, address=None, year=None):
-        prefixes = "PREFIX ns1: <http://schema.org/Person#> .\n" \
-                   "PREFIX ns2: <http://purl.org/dc/terms/> .\n" \
-                   "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n" \
-                   "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n" \
-                   "PREFIX xml: <http://www.w3.org/XML/1998/namespace> .\n" \
-                   "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> .\n"
+        prefixes = "PREFIX ns1: <http://schema.org/Person#>\n" \
+                   "PREFIX ns2: <http://purl.org/dc/terms/>\n" \
+                   "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" \
+                   "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" \
+                   "PREFIX xml: <http://www.w3.org/XML/1998/namespace>\n" \
+                   "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
 
         selectStatement = "SELECT ?surname ?forename ?title ?occupation ?address ?year\n"
         whereStatement = "WHERE {\n" \
@@ -30,34 +30,45 @@ class DatabaseQuerier:
                          "  OPTIONAL { ?d ns1:honorificPrefix ?title . }\n" \
                          "  OPTIONAL { ?d ns1:jobTitle ?occupation . }\n" \
                          "  OPTIONAL { ?d ns1:address ?address . }\n" \
-                         "  ?d ns2:issued ?year\n"
+                         "  ?d ns2:issued ?year .\n"
 
-        if surname:
-            whereStatement += "  ?d ns1:familyName {0} .\n".format(surname)
+        if surname is not None:
+            whereStatement += "  ?d ns1:familyName \"{0}\" .\n".format(surname)
 
-        if forename:
-            whereStatement += " ?d ns1:givenName {0} .\n".format(forename)
+        if forename is not None:
+            whereStatement += " ?d ns1:givenName \"{0}\" .\n".format(forename)
 
-        if title:
-            whereStatement += " ?d ns1:honorificPrefix {0} .\n".format(title)
+        if title is not None:
+            whereStatement += " ?d ns1:honorificPrefix \"{0}\" .\n".format(title)
 
-        if occupation:
-            whereStatement += " ?d ns1:jobTitle {0} .\n".format(occupation)
+        if occupation is not None:
+            whereStatement += " ?d ns1:jobTitle \"{0}\" .\n".format(occupation)
 
-        if address:
-            whereStatement += " ?d ns1:address {0} .\n".format(address)
+        if address is not None:
+            whereStatement += " ?d ns1:address \"{0}\" .\n".format(address)
 
-        if year:
-            whereStatement += " ?d ns2:issued {0} .\n".format(year)
+        if year is not None:
+            whereStatement += " ?d ns2:issued \"{0}\" .\n".format(year)
 
         whereStatement += "}"
 
         query = prefixes + selectStatement + whereStatement
 
-        print(query)
+        queryResult = self.graph.query(query)
+        formattedResult = []
 
-        return self.graph.query(query)
+        for record in queryResult:
+            formattedRecord = ()
+            for field in record:
+                if field is not None:
+                    formattedRecord += (field.value,)
+            formattedResult.append(formattedRecord)
+
+        return formattedResult
 
 if __name__ == "__main__":
     d = DatabaseQuerier()
-    print(d.query(surname="Abernethy"))
+    results = d.query(surname="Abernethy")
+
+    for r in results:
+        print(r)
