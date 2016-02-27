cc689bbaa1ffafba38a5ba9081bb41ac1e9e3622
Allan Brown <allanbrown239@gmail.com>
2016-02-27 14:37:55 +0000

    Added ability to query database by block key value.

--- DBMS/databaseQuerier.py
+++ DBMS/databaseQuerier.py
@@ -12,49 +12,55 @@ class DatabaseQuerier:
         self.graph.parse(database, format="turtle")
 
     def query(self, surname=None, forename=None, title=None, occupation=None, address=None, latitude=None,
-              longitude=None, year=None):
-        prefixes = "PREFIX ns1: <http://schema.org/Person#>\n" \
-                   "PREFIX ns2: <http://schema.org/GeoCoordinates#>\n" \
-                   "PREFIX ns3: <http://purl.org/dc/terms/>\n" \
+              longitude=None, year=None, bkv=None):
+        prefixes = "PREFIX person: <http://schema.org/Person#>\n" \
+                   "PREFIX geo: <http://schema.org/GeoCoordinates#>\n" \
+                   "PREFIX dcat: <http://purl.org/dc/terms/>\n" \
+                   "PREFIX pod: <file:///home/allan/Documents/University/Fourth-Year/PROJ/nls-semantic-tagging/" \
+                   "database/postOfficeRecord.owl%23>\n" \
                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" \
                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" \
                    "PREFIX xml: <http://www.w3.org/XML/1998/namespace>\n" \
                    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
 
-        selectStatement = "SELECT ?surname ?forename ?title ?occupation ?address ?lat ?lon ?year\n"
+        selectStatement = "SELECT ?surname ?forename ?title ?occupation ?address ?lat ?lon ?year ?bkv\n"
         whereStatement = "WHERE {\n" \
-                         "  ?d ns1:familyName ?surname .\n" \
-                         "  OPTIONAL { ?d ns1:givenName ?forename . }\n" \
-                         "  OPTIONAL { ?d ns1:honorificPrefix ?title . }\n" \
-                         "  OPTIONAL { ?d ns1:jobTitle ?occupation . }\n" \
-                         "  OPTIONAL { ?d ns1:address ?address . }\n" \
-                         "  OPTIONAL { ?d ns2:latitude ?lat . }\n"\
-                         "  OPTIONAL { ?d ns2:longitude ?lon . }\n"\
-                         "  ?d ns3:issued ?year .\n"
+                         "  ?d person:familyName ?surname .\n" \
+                         "  OPTIONAL { ?d person:givenName ?forename . }\n" \
+                         "  OPTIONAL { ?d person:honorificPrefix ?title . }\n" \
+                         "  OPTIONAL { ?d person:jobTitle ?occupation . }\n" \
+                         "  OPTIONAL { ?d person:address ?address . }\n" \
+                         "  OPTIONAL { ?d geo:latitude ?lat . }\n" \
+                         "  OPTIONAL { ?d geo:longitude ?lon . }\n" \
+                         "  ?d dcat:issued ?year .\n" \
+                         "  ?d pod:blockKeyValue ?bkv .\n"
 
         if surname is not None:
-            whereStatement += "  ?d ns1:familyName \"{0}\" .\n".format(surname)
+            whereStatement += "  ?d person:familyName \"{0}\" .\n".format(surname)
 
         if forename is not None:
-            whereStatement += " ?d ns1:givenName \"{0}\" .\n".format(forename)
+            whereStatement += " ?d person:givenName \"{0}\" .\n".format(forename)
 
         if title is not None:
-            whereStatement += " ?d ns1:honorificPrefix \"{0}\" .\n".format(title)
+            whereStatement += " ?d person:honorificPrefix \"{0}\" .\n".format(title)
 
         if occupation is not None:
-            whereStatement += " ?d ns1:jobTitle \"{0}\" .\n".format(occupation)
+            whereStatement += " ?d person:jobTitle \"{0}\" .\n".format(occupation)
 
         if address is not None:
-            whereStatement += " ?d ns1:address \"{0}\" .\n".format(address)
+            whereStatement += " ?d person:address \"{0}\" .\n".format(address)
 
         if latitude is not None:
-            whereStatement += " ?d ns2:latitude \"{0}\" .\n".format(latitude)
+            whereStatement += " ?d geo:latitude \"{0}\" .\n".format(latitude)
 
         if longitude is not None:
-            whereStatement += " ?d ns2:longitude \"{0}\" .\n".format(longitude)
+            whereStatement += " ?d geo:longitude \"{0}\" .\n".format(longitude)
 
         if year is not None:
-            whereStatement += " ?d ns3:issued \"{0}\" .\n".format(year)
+            whereStatement += " ?d dcat:issued \"{0}\" .\n".format(year)
+
+        if bkv is not None:
+            whereStatement += " ?d pod:blockKeyValue \"{0}\" .\n".format(bkv)
 
         whereStatement += "}"
 
@@ -62,15 +68,15 @@ class DatabaseQuerier:
 
         queryResult = self.graph.query(query)
         formattedResult = []
-        fields = ["surname", "forename", "title", "occupation", "address", "latitude", "longitude", "year"]
+        fields = ["surname", "forename", "title", "occupation", "address", "latitude", "longitude", "year", "bkv"]
 
         for record in queryResult:
-            formattedRecord = {}
+            formattedRecord = []
             for i, field in enumerate(record):
                 if field is not None:
-                    formattedRecord[fields[i]] = field.value
+                    formattedRecord.append((fields[i], field.value))
                 else:
-                    formattedRecord[fields[i]] = None
+                    formattedRecord.append((fields[i], None))
 
             formattedResult.append(formattedRecord)
 
@@ -109,13 +115,17 @@ if __name__ == "__main__":
         print(r)
     print("")
 
-    print("COORDS:")
+    print("YEAR:")
     results = d.query(latitude="55.9128487", longitude="-3.1615066")
+    for r in results:
+        print(r)
+
+    results = d.query(year="1851")
     for r in results:
         print(r)
     print("")
 
-    print("YEAR:")
-    results = d.query(year="1851")
+    print("BKV:")
+    results = d.query(bkv="ABE90KIR")
     for r in results:
         print(r)
