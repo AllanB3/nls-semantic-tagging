44c4d8a8279880ae838a5f4605179e3ca3b468f0
Allan Brown <allanbrown239@gmail.com>
2016-03-15 03:01:52 +0000

    Removed unnecessary OCR script.

--- OCR/extract_alphabetical_indexes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Extract alphabetical index"
-pdftk 85920425.6-BW-1910.pdf cat 45-482 output 85920425.6-BW-1910-ALPHA-INDEX.pdf
-pdftk 85920425.23-RGB-1910.pdf cat 45-482 output 85920425.23-RGB-1910-ALPHA-INDEX.pdf
